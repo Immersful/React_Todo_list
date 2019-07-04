8e9395cb5860d1f84ce5a6931c35ce8f39fac07f
Christopher Simpson <ImmersfulFlame@gmail.com>
2019-07-04 14:20:43 -0400

    Todo

---
+++ precache-manifest.ad9392f46a48760d51796e5f06347d43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02895efd3c1825fe85d8e63b655bf8a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70d1631021fb40656236",
+    "url": "/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "0bbef7593c1e3ae88319",
+    "url": "/static/js/2.ef006c3a.chunk.js"
+  },
+  {
+    "revision": "70d1631021fb40656236",
+    "url": "/static/js/main.61ddcecb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
